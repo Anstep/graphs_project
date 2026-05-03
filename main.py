fbcb6c4fbbf1d7e0ef962ad143125ff63d204a77
Anstep <anstep01@gmail.com>
2026-05-03 21:53:11 +0800

    Created simple welcome page

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hi")
+import streamlit as st
 
+st.set_page_config(page_title="Графы", layout="centered")
+st.markdown("""
+**Добро пожаловать!**
+
+*Разработано в рамках курса «Дискретные структуры».*
+""")
+st.info("👈 Выберите раздел в боковой панели, чтобы начать.")
 
 if __name__ == "__main__":
     pass
